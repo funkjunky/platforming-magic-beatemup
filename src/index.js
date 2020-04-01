486276e4804cfb1815cf7c325b61dcf0fc5a3be9
Jason McCarrell <jason.abz@gmail.com>
2020-03-31 23:35:45 -0400

    image showing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'regenerator-runtime/runtime.js';
+import 'end-polyFills';
+
 import { createStore, applyMiddleware } from 'redux';
 import { createYieldEffectMiddleware } from 'redux-yield-effect';
 import { put, fork, join } from 'redux-yield-effect/lib/effects';
@@ -5,37 +8,49 @@ import { tickMiddleware, resumeTicks, pauseTicks } from 'effect-tick';
 import { createEntity } from './entities/index.js';
 import reducer from './reducer.js';
 import metaSelector from 'redux-meta-selector';
+import loadResources from './loadResources';
 import graphics from './graphics.js';
-import 'end-polyFills';
 
 document.addEventListener('DOMContentLoaded', start);
 
-if(module.hot) {
-    module.hot.accept(() => {
-        window.store.replaceReducer(reducer)
-        window.cancelAnimationFrame(window.raf);
-        window.raf = window.requestAnimationFrame(step)
-    });
-}
+const setRAF = resources => window.requestAnimationFrame(step(resources));
 
-function step(dt) {
-    graphics(window.ctx, window.store.getState(), dt);
-    window.requestAnimationFrame(step);
+const step = resources => dt => {
+  graphics(window.ctx, window.store.getState(), resources, dt);
+  window.requestAnimationFrame(step(resources));
 };
 
-function start() {
-    window.store = createStore(
-        reducer,
-        applyMiddleware(
-            createYieldEffectMiddleware(),
-            tickMiddleware,
-            metaSelector
-        ),
-    );
-    store.dispatch(resumeTicks());
+if(module.hot) {
+  // TODO: if this ever gets slower,
+  // then look into doing intelligent reloading based on what changed.
+  module.hot.accept(() => {
+    window.store.replaceReducer(reducer);
+    window.cancelAnimationFrame(window.raf);
 
-    window.ctx = document.querySelector('canvas').getContext('2d');
-    window.raf = window.requestAnimationFrame(step);
+    // do this after, so it doesn't delay the other things from loading.
+    // TODO: do this more intelligently... so only if resources have changed.
+    const resources = loadResources();
+    setRAF(resources);
+  });
+}
 
-    store.dispatch(createEntity({ x: 100 }));
+async function start() {
+  console.log('loading...');
+  const resources = await loadResources();
+  console.log('resources: ', resources);
+
+  window.store = createStore(
+    reducer,
+    applyMiddleware(
+      createYieldEffectMiddleware(),
+      tickMiddleware,
+      metaSelector
+    ),
+  );
+  store.dispatch(resumeTicks());
+
+  window.ctx = document.querySelector('canvas').getContext('2d');
+  setRAF(resources);
+
+  store.dispatch(createEntity({ x: 100 }));
 };
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   stats: {
