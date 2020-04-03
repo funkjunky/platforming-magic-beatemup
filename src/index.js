0e1ae00d3e0a338487848114062a979245e63fd9
Jason McCarrell <jason.abz@gmail.com>
2020-04-03 16:26:39 -0400

    fixed up the HMR a little, but not working

--- src/index.js
+++ src/index.js
@@ -13,29 +13,36 @@ import graphics from './graphics.js';
 
 document.addEventListener('DOMContentLoaded', start);
 
-const setRAF = resources => window.requestAnimationFrame(step(resources));
+const setRAF = resources => window.raf = window.requestAnimationFrame(step(resources));
 
 const step = resources => dt => {
   graphics(window.ctx, window.store.getState(), resources, dt);
   window.requestAnimationFrame(step(resources));
 };
 
+let a = 0;
+
+console.log('modue.hot: ', module.hot);
 if(module.hot) {
+  console.log('called: ', ++a);
+  //module.hot.accept(async () => {
+  module.hot.accept('./graphics.js', async () => {
   // TODO: if this ever gets slower,
   // then look into doing intelligent reloading based on what changed.
-  module.hot.accept(() => {
+    console.log('window raf: ', window.raf);
     window.store.replaceReducer(reducer);
     window.cancelAnimationFrame(window.raf);
 
     // do this after, so it doesn't delay the other things from loading.
     // TODO: do this more intelligently... so only if resources have changed.
-    const resources = loadResources();
+    const resources = await loadResources();
+    console.log('setting new raf');
     setRAF(resources);
+    setupController();
   });
 }
 
 async function start() {
-  console.log('loading...');
   const resources = await loadResources();
   console.log('resources: ', resources);
 
@@ -52,5 +59,15 @@ async function start() {
   window.ctx = document.querySelector('canvas').getContext('2d');
   setRAF(resources);
 
-  window.store.dispatch(createEntity({ x: 100 }));
+  window.store.dispatch(createEntity({ x: 50, y: 480 - 96 }));
+
+  setupController();
+}
+
+const setupController = () => {
+  // setup controls
+  // TODO: move somewhere else????
+  window.addEventListener('gamepadconnected', e => {
+    console.log('gamepad connected: ', e.gamepad);
+  });
 }
