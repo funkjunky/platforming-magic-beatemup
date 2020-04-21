5ac385b1a4555c222bb0dae17bcc6452507a9157
Jason McCarrell <jason.abz@gmail.com>
2020-04-21 08:32:29 -0400

    added block to test falling of edge

--- src/entities/level.js
+++ src/entities/level.js
@@ -4,5 +4,7 @@ export const level = [
   { x: 0, y: 480-spriteWidth, width: 640, height: spriteWidth },
   { x: 0, y: 480-spriteWidth-spriteWidth, width: spriteWidth, height: spriteWidth },
   { x: 640-spriteWidth, y: 480-spriteWidth-spriteWidth, width: spriteWidth, height: spriteWidth },
+
   { x: spriteWidth, y: spriteWidth, width: spriteWidth, height: spriteWidth },
+  { x: spriteWidth * 3, y: 480 - spriteWidth * 2, width: spriteWidth, height: spriteWidth },
 ];
