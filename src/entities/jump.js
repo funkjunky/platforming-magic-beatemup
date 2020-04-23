5df6d98ee193632464577ebfe3c93c83cca2e265
Jason McCarrell <jason.abz@gmail.com>
2020-04-23 08:10:11 -0400

    No jumping while falling

--- src/entities/jump.js
+++ src/entities/jump.js
@@ -1,11 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Note: here, the reducer is enforcing the state machine
 const jumpSlice = createSlice({
   name: 'jump',
   initialState: 'falling',
   reducers: {
-    grounded: () => 'grounded', //currently unused. see notes in cleanupAction.js
-    jumping: () => 'jumping',
+    grounded: () => 'grounded',
+    jumping: state => state === 'grounded' ? 'jumping' : undefined, // undefined means unchanged,
     falling: () => 'falling',
   },
 });
